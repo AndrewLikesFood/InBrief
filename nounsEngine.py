141beba2c7f82d6f5d056a66661d3dff19d90073
Andrew Wang <zepianistandrew@gmail.com>
2015-11-14 15:38:09 -0800

    Basic noun engine implementation complete.

--- nounsEngine.py
+++ nounsEngine.py
@@ -1,1 +1,28 @@
 import json
+import rake
+import operator
+import urllib
+
+from HTMLParser import HTMLParser
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+def retrieveWikiArticle(keyword):
+	url = "https://en.wikipedia.org/w/api.php?action=parse&prop=text&format=json&page=" + urllib.quote_plus(keyword)
+	response = urllib.urlopen(url)
+	data = json.loads(response.read())
+	rake_object = rake.Rake("SmartStoplist.txt", 5, 3, 4)
+	keywords = rake_object.run(strip_tags(data['parse']['text']['*']))
+	print keywords
